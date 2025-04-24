9eb4aff24bd7e7f8185f04335fdd0d9a1c97c7ad
cristian-sudo <cplop21@gmail.com>
2025-04-25 00:05:11 +0100

    :zap: Fixed the create new company modal

--- resources/views/livewire/companies.blade.php
+++ resources/views/livewire/companies.blade.php
@@ -8,7 +8,7 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="flex justify-between items-center mb-6">
                         <h2 class="text-2xl font-semibold text-gray-900 dark:text-white">Companies</h2>
-                        <button x-on:click="$dispatch('open-modal')"
+                        <button wire:click="create"
                                 class="inline-flex items-center px-4 py-2 bg-indigo-600 dark:bg-indigo-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 dark:hover:bg-indigo-600 focus:bg-indigo-700 dark:focus:bg-indigo-600 active:bg-indigo-900 dark:active:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                             {{ __('New Company') }}
                         </button>
